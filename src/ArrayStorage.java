5b331e69632734f9c1a3dcd868fd99e58e6bdc86
ElenaNas <nas-elena@yandex.ru>
2023-12-29 08:25:51 -0500

    class ArrayStorage and realisation of its methods (clear(), save(), get(), delete(), getAll(), size())

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,60 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (Resume value : storage) {
+            if (value != null && value.uuid.equals(uuid)) {
+                resume = value;
+                break;
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (this.storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                for (int j = i; j < storage.length - 1; j++) {
+                    storage[j] = storage[j + 1];
+                }
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int firstNullValue = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                firstNullValue = i;
+                break;
+            }
+        }
+        return Arrays.copyOfRange(storage, 0, firstNullValue);
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
