d17926bd3146fd1b3bc3c0b2995aa779dcb4a12b
ElenaNas <nas-elena@yandex.ru>
2024-02-10 09:01:03 -0500

    Changes into FileStorage and PathStorage classes; interface IStreamStrategy and ObjectStorage class realized to conform with Strategy pattern. Test classes added accordingly.

--- test/webapp/storage/ObjectStreamStorageTest.java
+++
@@ -1,8 +0,0 @@
-package webapp.storage;
-
-class ObjectStreamStorageTest extends AbstractStorageTest {
-
-    public ObjectStreamStorageTest() {
-        super(new ObjectStreamStorage(STORAGE_DIRECTORY));
-    }
-}
