62715f53e31e0e9117c6dce64cafd832386de99a
ElenaNas <nas-elena@yandex.ru>
2024-02-11 02:01:21 -0500

    Use of constant variable instead of String in constructors.

--- test/webapp/storage/PathStorageTest.java
+++ test/webapp/storage/PathStorageTest.java
@@ -3,6 +3,6 @@ package webapp.storage;
 
 class PathStorageTest extends AbstractStorageTest {
     public PathStorageTest() {
-        super(new PathStorage("C:\\Users\\nas-e\\basejava\\src\\webapp\\storageSer", new ObjectStrategyStorage()));
+        super(new PathStorage(STORAGE_DIRECTORY.getAbsolutePath(), new ObjectStrategyStorage()));
     }
 }
