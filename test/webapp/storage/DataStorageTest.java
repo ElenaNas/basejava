82b2efd248ba2f24ee54d25360bde0fc6d4833ff
ElenaNas <nas-elena@yandex.ru>
2024-05-05 19:24:03 +0200

    Minor changes before finalizing the project.

--- test/webapp/storage/DataStorageTest.java
+++ test/webapp/storage/DataStorageTest.java
@@ -2,9 +2,9 @@ package webapp.storage;
 
 import webapp.storage.strategy.DataStreamStorage;
 
-class DataStorageTest extends AbstractStorageTest {
 
-    public DataStorageTest() {
-        super(new PathStorage(STORAGE_DIRECTORY.getPath(), new DataStreamStorage()));
+class DataStorageTest extends AbstractStorageTest {
+public DataStorageTest()  {
+        super(new PathStorage(STORAGE_DIRECTORY.getAbsolutePath(), new DataStreamStorage()));
     }
 }
